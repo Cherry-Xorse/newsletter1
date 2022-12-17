74a167a1e8e6de33bfe57e732180cc74f6aa558a
Cherry Xorse <92997463+Cherry-Xorse@users.noreply.github.com>
2022-12-16 23:13:44 -0800

    version 2,ps- i would like to add more specific messages for errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require("express");
 const bodyParser= require("body-parser");
 const request=require("request")
+const https = require("https")
 const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
@@ -15,45 +16,86 @@ res.sendFile(__dirname +"/signUp.html")
 
 app.post("/", function(req,res){
 
-   var firstName= req.body.fname;
-   var lastName=req.body.lname;
-   var email =req.body.email;
+   const firstName= req.body.fname;
+   const lastName=req.body.lname;
+   const email =req.body.email;
+
+  const data={
+     members: [{
+       email_address: email,
+       status:"subscribed",
+       merge_fields:{
+         FNAME:firstName,
+
+         LNAME:lastName
+       }
+     }],
+   };
+
+   const jsonData = JSON.stringify(data);
+
+   const url = "https://us14.api.mailchimp.com/3.0/lists/d8eff9914a";
+   const options = {
+     method: "POST",
+     auth: "cherry:ab2ecec261fb48ab3cc801717ef7b4bb-us14"
+
+   }
+
+   const request = https.request(url,options, function(response){
+
+     if (response.statusCode === 200) {
+         res.sendFile(__dirname + "/success.html")
+       } else {
+         res.sendFile(__dirname + "/failure.html")
+       }
+
+     response.on("data",function(data){
+       console.log(JSON.parse(data));
+     });
+   });
+
+   request.write(jsonData);
+   request.end();
 
 
 
-client.setConfig({
-  apiKey: "ab2ecec261fb48ab3cc801717ef7b4bb-us14",
-  server: "us14"
-});
+// client.setConfig({
+//   apiKey: "ab2ecec261fb48ab3cc801717ef7b4bb-us14",
+//   server: "us14"
+// });
 
-const run = async () => {
-  const response = await client.lists.batchListMembers("d8eff9914a", {
-    members: [{
-      email_address: email,
-      status:"subscribed",
-      merge_fields:{
-        FNAME:firstName,
-
-        LNAME:lastName
-      }
-    }],
-  });
-
-  if (response.status === 202) {
-    res.send(__dirname + "/sucess.html")
-  } else {
-    res.sendFile(__dirname + "/failure.html")
-  }
-  console.log(response);
-};
-
-run();
+// const run = async () => {
+//   const response = await client.lists.batchListMembers("d8eff9914a", {
+//     members: [{
+//       email_address: email,
+//       status:"subscribed",
+//       merge_fields:{
+//         FNAME:firstName,
+//
+//         LNAME:lastName
+//       }
+//     }],
+//   });
+//
+//   if (response.status === 200) {
+//     res.sendFile(__dirname + "/success.html")
+//   } else {
+//     res.sendFile(__dirname + "/failure.html")
+//   }
+//   console.log(response);
+// };
+//
+// run();
 
 
 
  })
 
-app.listen(3000,function(){
+ app.post("/failure",function(req,res){
+   res.redirect("/")
+ })
+
+app.listen(process.env.PORT|| 3000,function(){
   console.log("here is holy")
 })
 
